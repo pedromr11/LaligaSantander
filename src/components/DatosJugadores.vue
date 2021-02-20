d0c3c422421296ec9a4e80f8a90fc5fef9936d7f
Pedro Muñoz <pedromurivas11@gmail.com>
2021-02-20 18:34:11 +0100

    Tabla de equipos y 7.1 completo

---
+++ src/components/DatosJugadores.vue
@@ -0,0 +1,37 @@
+<template>
+    
+    <h2>Jugadores</h2>        
+    <ul v-for="(jugador, goles) in laliga" :key="goles">
+        <li v-if="this.nombreJugadores == jugador.team">{{jugador.name}}</li>
+    </ul> 
+
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+    props: [
+        "nombreJugadores"
+    ],
+    data() {
+        return {
+            laliga: []
+        }
+    },
+    mounted() {
+        axios 
+            .get('http://localhost:3000/players')
+            .then(response => {
+              this.laliga = response.data
+            })        
+    }
+}
+</script>
+
+<style>
+li{
+    list-style-type: none;
+}
+</style>
--- src/views/Jugadores.vue
+++ src/views/Jugadores.vue
@@ -1,5 +1,80 @@
 <template>
   <div class="jugadores">
     <h1>Jugadores:</h1>
+
+    <div class="tabla">
+    <table>
+      <tr>
+        <th>Equipo</th>
+      </tr>
+
+      <tr v-for="(equipo, index) in equipos" :key="index">
+        <td @click="obtenerJugadores(equipo.name)">           
+          {{equipo.name}} 
+        </td>
+      </tr>
+    </table>    
+
+  </div>
+
+  <div class="datos" v-if="jugadores != ''">
+    
+    <DatosJugadores :nombreJugadores="jugadores" />
+
+  </div>
   </div>
 </template>
+
+<script>
+
+import axios from "axios";
+
+import DatosJugadores from '@/components/DatosJugadores.vue';
+
+export default {
+  data(){
+    return {
+      equipos: [],
+      jugadores: ""
+    }
+  },
+  created() {
+     axios
+      .get('http://localhost:3000/clubs')
+      .then(response => {
+      this.equipos = response.data;
+    })
+  },
+  components: {
+      DatosJugadores
+    },
+  methods: {
+    obtenerJugadores: function(nombreEquipo){
+     this.jugadores = nombreEquipo;
+    }
+  }    
+
+}
+</script>
+
+<style scoped>
+.tabla{
+  position: absolute;
+}
+
+.tabla, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  margin-left: 30%;
+  width: 20%;
+}
+
+th, td {
+  padding: 10px;
+}
+
+.datos{
+    margin-left: 60%;
+    width: 15%;    
+}
+</style>
