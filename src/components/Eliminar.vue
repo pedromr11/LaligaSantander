a65b8f5a69098b3e400ac3ead35fe4a4a9e05526
Pedro Muñoz <pedromurivas11@gmail.com>
2021-02-23 19:48:36 +0100

    Punto 9 bloquea boton pero no elimina

--- src/components/Eliminar.vue
+++ src/components/Eliminar.vue
@@ -5,7 +5,7 @@
             <option :value="elemento.name" v-for="(elemento, index) in jugadores" :key="index">{{elemento.name}}</option>
         </select>
         <br><br>
-        <input type="submit" name="borrar" value="borrar jugador" @click="borrar(name)">
+        <button :disabled="name === ''" @click="borrar(name)"> Eliminar Jugador </button>
     </div>
 </template>
 
